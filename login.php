4ccd1edabbd052855f4194fe7e37afb1e8a9cdc8
root <root@CSE127>
2014-10-20 03:03:36 -0700

    fix in other os

--- login.php
+++ login.php
@@ -40,13 +40,13 @@ if(isset($_POST['NEW']))
 	// user account and send him to view.php.
 	$query = pg_query($con, "SELECT username FROM poster WHERE username='$username'");
 	if(!$query)
-		// handle error
-	$row = pg_fetch_row($query);
-	if(!$row)
 	{
+		// handle error
 		$stmt = "INSERT INTO poster(username, password) VALUES('$username', '$password')";
-		pg_query($con, $stmt) or die ("Failed to execute: $stmt\n");
+		$query = pg_query($con, $stmt);
+		if($query)
 		header("Location: view.php?user=$username");
+
 	}
 	else
 	{
